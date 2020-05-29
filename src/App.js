c9a1305d9a31dc8b02841bd95350c2e64b214c6b
Britney Smith <brit.smith7@gmail.com>
2020-05-29 13:57:15 -0400

    Fetch current weather data by user lat lon

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
+const apiKey = process.env.REACT_APP_API_KEY;
+
 function App() {
+
+  //openweather apt: receiving object with key "weather"
+  const [coordinates, setCoordinates] = useState({
+    lat: null,
+    lon: null
+  });
+  function locationSuccess(positionObj) {
+    console.log(`${positionObj.coords.latitude}. ${positionObj.coords.longitude}`);
+    setCoordinates({
+      lat: positionObj.coords.latitude,
+      lon: positionObj.coords.longitude
+    });
+  }
+
+  function locationError(errObj) {
+    console.log(`${errObj.code}: ${errObj.message}`);
+  }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(locationSuccess, locationError);
+  }, []);
+
+  useEffect(() => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`)
+      .then(res => {
+        if (res.ok) {
+          console.log(res.body);
+          res.json();
+        }
+      })
+      .then(data => {
+        console.log(data);
+      })
+      .catch (err => {
+        console.log(err);
+      })
+  },[coordinates])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hi
     </div>
   );
 }
